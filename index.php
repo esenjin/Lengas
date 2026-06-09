d841b4451d1c9d2daedc13e9422c61d579aeadd4
Esenjin <esenjin@sangigi-fuchsia.fr>
2026-06-09 12:26:56 +0200

    nouveau chemin correct de "favicon.ico" dans toutes les occurences de ce dernier à "index.php"

--- index.php
+++ index.php
@@ -158,7 +158,7 @@ if ($options['private_mode']) {
         <title><?= htmlspecialchars($options['index_page_title']) ?></title>
         <meta name="description" content="<?= htmlspecialchars($options['site_description']) ?>">
         <meta property="og:image" content="logo.png">
-        <link rel="icon" type="image/x-icon" href="favicon.ico">
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
         <link rel="stylesheet" href="assets/css/main.css">
     </head>
     <body>
@@ -239,7 +239,7 @@ function get_latest_version_from_gitea() {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($options['index_page_title']) ?></title>
     <meta name="description" content="<?= htmlspecialchars($options['site_description']) ?>">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
     <link rel="stylesheet" href="assets/css/main.css">
     <style>
         /* Style pour les cartes cliquables */
