d6c127c12354e04b0b85b599770fcc9049792352
Esenjin <esenjin@sangigi-fuchsia.fr>
2026-06-21 20:25:51 +0200

    màj n° de version

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Configuration du site
-define('SITE_VERSION', '3.6.0');
+define('SITE_VERSION', '3.6.1');
 define('URL_GITEA', 'https://git.crystalyx.net/Esenjin_Asakha/Lengas');
 
 // Chemin vers la base de données SQLite
